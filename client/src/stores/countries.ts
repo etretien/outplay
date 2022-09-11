0fc384b336306e3fe5ffbdf378ebbb0dbd91abfc
ichigrinova <ichigrinova@calltouch.net>
2022-09-11 15:49:49 +0300

    countries moved to json

--- client/src/stores/countries.ts
+++ client/src/stores/countries.ts
@@ -1,12 +1,10 @@
 import { action, atom } from 'nanostores';
-import axios from 'axios';
+
+import json from '../static/countries.json';
 
 export const countries = atom<{ [field: string]: string }>({});
 
 export const getCountries = action(countries, 'getCountries', async (store) => {
-  const { pathname } = document.location;
-  const path = pathname.length > 1 ? pathname.split('/')[1] : '';
-  const response = await axios.get<null, { data: string }>(`${path}/data/countries.json`);
-  store.set(JSON.parse(response.data));
+  store.set(JSON.parse(json));
   return store.get();
 });
